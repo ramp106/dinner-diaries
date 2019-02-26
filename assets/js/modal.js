326e3bcf1a178ab882e3be52e223a85b02ce226f
isabelleme <33831160+isabelleme@users.noreply.github.com>
2019-02-26 15:45:09 +0100

    changes host

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -101,3 +101,29 @@ window.onclick = function(event) {
     modal4.style.display = "none";
   }
 }
+
+// Get the modal
+var modal5 = document.getElementById('myModal5');
+
+// Get the button that opens the modal
+var btn5 = document.getElementById("myBtn5");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close5")[0];
+
+// When the user clicks the button, open the modal 
+btn5.onclick = function() {
+  modal5.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal5.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+}
